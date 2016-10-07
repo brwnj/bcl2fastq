5b094cb43923897947cff21c8e1219920f5f8088
Joe Brown <joe.brown@pnnl.gov>
2016-10-07 15:47:56 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='bcl2fastq',
-    version='1.2.2',
+    version='1.2.3',
     url='http://github.com/brwnj/bcl2fastq',
     license='MIT',
     author='Joe Brown',
