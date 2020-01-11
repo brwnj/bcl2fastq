76ef290c6d0859e626d5eb8c94a9417fb0f2824d
Joe Brown <brwnjm@gmail.com>
2020-01-11 16:30:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='bcl2fastq',
-    version='1.2.5',
+    version='1.2.6',
     url='http://github.com/brwnj/bcl2fastq',
     license='MIT',
     author='Joe Brown',
